7e13facb0aef8388e148c04c9ffc5768e3ea5a5c
EricNgo1972 <ngo-thanh.tung@spc-technology.com>
2021-03-20 05:25:07 -0400

    4.7.6.923

--- PhoebusCmd/My Project/AssemblyInfo.vb
+++ PhoebusCmd/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("4.7.6.813")>
-<Assembly: AssemblyFileVersion("4.7.6.813")>
+<Assembly: AssemblyVersion("4.7.6.923")>
+<Assembly: AssemblyFileVersion("4.7.6.923")>
